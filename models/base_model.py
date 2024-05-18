9b7f2969fad82908251cbaa98eac2cb16261e769
Temitayo <temfoden@gmail.com>
2024-05-18 13:45:45 +0100

    add base model file content

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        """Initializes the baseModel with unique Id and current datetime"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at()
+
+    def __str__(self):
+        """Returns the string representation of the baseModel"""
+        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
+
+    def save(self):
+        """Updates the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns the dictionary representation of my baseModel"""
+        base_dict = self.__dict__.copy()
+        base_dict['__class__'] = self.__class__.__name__
+        base_dict['created_at'] = self.created_at.isoformat()
+        base_dict['updated_at'] = self.updated_at.isoformat()
+        return base_dict
