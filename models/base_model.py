4f9da626477e959cb674527062126235f2c98398
Temitayo <temfoden@gmail.com>
2024-05-18 17:56:56 +0100

    add functionality for baseModel to accept dictionary parameter

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,18 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes the baseModel with unique Id and current datetime."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns the string representation of the baseModel."""
