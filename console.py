50cbb8cac0f4fa3071f8103c1a0dd35a3c2a8a70
Temitayo <temfoden@gmail.com>
2024-05-19 11:40:31 +0100

    add console module that contains entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,23 @@
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the HBNB project."""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program.\n"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        print()  # Ensure a new line after EOF is entered
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line."""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
