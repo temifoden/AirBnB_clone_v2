02285af8d029e7169c7a2c304d8886a929e13a37
Temitayo <temfoden@gmail.com>
2024-06-29 02:22:30 +0100

    update: base storage file to use db storage

--- models/old_base_model.py
+++ models/old_base_model.py
@@ -1,17 +1,8 @@
 import uuid
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DateTime
-import models
-
-Base = declarative_base()
 
 
 class BaseModel:
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-
     def __init__(self, *args, **kwargs):
         """Initializes the baseModel with unique Id and current datetime."""
         if kwargs:
@@ -24,6 +15,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
         """Returns the string representation of the baseModel."""
@@ -32,9 +25,8 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
-  
+        from models import storage
+        storage.save()
 
     def to_dict(self):
         """Returns the dictionary representation of my baseModel."""
@@ -42,10 +34,4 @@ class BaseModel:
         base_dict['__class__'] = self.__class__.__name__
         base_dict['created_at'] = self.created_at.isoformat()
         base_dict['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in base_dict:
-            del base_dict['_sa_instance_state']
         return base_dict
-    
-    def delete(self):
-        """Deletes the current instance from the storage."""
-        models.storage.delete(self)
